691d6ada7e0053e55e91ba66851ae5326cfd18d6
rsivapr <rohit.sprasad@gmail.com>
2013-07-07 06:33:21 -0400

    Updated reqHand with non-blocking ops

--- router.js
+++ router.js
@@ -1,5 +1,13 @@
-function route(pathname) {
+function route(handle, pathname, res) {
 	console.log("About to route a request for " + pathname);
+	if (typeof handle[pathname] == 'function') {
+		handle[pathname](res);
+	} else {
+		console.log("No request handler found for " + pathname);
+                res.writeHead(200, {'content-type':'text/plain'});
+                res.write("404 Not Found.");
+                res.end();			
+	}
 }
 
 exports.route = route;
--- server.js
+++ server.js
@@ -1,15 +1,11 @@
 var http = require('http');
 var url = require('url');
 
-function start(route) {
+function start(route, handle) {
 	function onRequest(req, res) {
 		var pathname = url.parse(req.url).pathname;
 		console.log("Request for " + pathname + " received.");		
-		route(pathname);
-
-		res.writeHead(200, {'content-type':'text/plain'});
-		res.write('Hellow hemaant!!');
-		res.end();
+		route(handle, pathname, res);
 	}
 
 	http.createServer(onRequest).listen(8888);
