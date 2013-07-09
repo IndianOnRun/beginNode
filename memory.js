27af527c9de0b5f241409bfede24c3d559e5b2d9
Rohit Sivaprasad <rohit.sprasad@gmail.com>
2013-07-09 19:44:17 -0400

    BROKEN

--- memory.js
+++ memory.js
@@ -5,13 +5,6 @@ var BSON = require('mongodb').BSON;
 var ObjectID = require('mongodb').ObjectID;
 var MongoClient = require('mongodb').MongoClient;
 
-var roger = {};
-
-var rohit = function(docs) {
-	roger = docs;
-};
-
-
 var conn = MongoClient.connect('mongodb://localhost:27017/rewrite', function(err, db) {
 	if(err) throw err;
 	rohit();	
