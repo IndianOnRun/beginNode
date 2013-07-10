ccd9b517b948fa8f90c4073ed243d1550a4cf645
Rohit Sivaprasad <rohit.sprasad@gmail.com>
2013-07-10 17:01:37 -0400

    Delete mong.js

--- mong.js
+++
@@ -1,5 +0,0 @@
-var MongoClient = require('mongodb').MongoClient;
-
-MongoClient.connect("mongodb://localhost:27017/integration_test", function(err, db) {
-  db.collection("replicaset1").update({a:1}, {b:1}, {upsert:true}, function(err, result) {});
-});
